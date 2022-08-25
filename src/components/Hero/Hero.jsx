a3204271ed77a9354698dc062d353f767c2379a9
Farhan Masood Khan <farhanmasoodkhan2@gmail.com>
2022-08-25 15:29:50 +0500

    first

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,101 @@
+import React from 'react';
+import Header from '../Header/Header';
+import './Hero.css';
+
+import hero_image from '../../assets/hero_image.png';
+import hero_image_back from '../../assets/hero_image_back.png';
+import heart from '../../assets/heart.png';
+import calories from '../../assets/calories.png';
+
+
+const Hero = () => {
+  return (
+  <div className='hero'>
+
+
+    <div className="left-h">
+
+      {/* Header */}
+        <Header/>
+
+      {/* The best ad */}
+        <div className="the-best-ad">
+          <div></div>
+          <span>The Best Fitness Club in the Town</span>
+        </div>
+
+      {/* Hero heading */}
+        <div className="hero-text">
+          <div>
+            <span className='stroke-text'>Shape </span>
+            <span>Your</span>
+          </div>
+
+          <div>
+            <span>Ideal body</span>
+          </div>
+          
+          <div>
+            <span>In here we will help you to shape and build your ideal body and live up your life to fullest</span>
+          </div>
+        </div>
+
+      {/* Figures */}
+        <div className="figures">
+          <div> 
+            <span>130+</span>
+            <span>Expert coaches</span>
+          </div>
+          <div> 
+            <span>1050+</span>
+            <span>members joined</span>
+          </div>
+          <div> 
+            <span>60+</span>
+            <span>Fitness Programs</span>
+          </div>
+        </div>
+      
+      {/* HERO BUTTONS */}
+        <div className="hero-buttons">
+          <buttons className="btn">Get Started</buttons>
+          <buttons className="btn">Learn More</buttons>
+        </div>
+
+
+    </div>
+      <div className="right-h">
+        {/* join now btn */}
+        <button className='btn'> Join Now</button>
+        {/* Heart rate box */}
+        <div className="heart-rate">
+          <img src={heart} alt="" />
+          <span>Heart Rate</span>
+          <span>120 BPM</span>
+        </div>
+
+        {/* Hero Imgs */}
+        <img src={hero_image} alt="" className='hero-image' />
+        <img src={hero_image_back} alt="" className='hero-image-back' />
+
+        {/* Calories Box */}
+
+        <div className="calories">
+          <img src={calories} alt="" />
+          <div>
+          <span>Calories Burned</span>
+          <span>500 kcal</span>
+          </div>
+        </div>
+
+      </div>
+
+  </div>
+
+
+
+  )
+}
+ 
+
+export default Hero;
---
+++ src/components/Plans/Plans.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import './Plans.css';
+import {plansData} from '../../data/plansData';
+import whitetick from '../../assets/whiteTick.png'
+
+
+const Plans = () => {
+  return (
+    <div className="Plans-container">
+        <div className="programs-header">
+            <span className='stroke-text'>Ready to Start</span>
+            <span> Your journey</span>
+            <span className='stroke-text'>now with us</span>
+        </div>
+
+        {/* Packages Cards */}
+        <div className="plans">
+          {plansData.map((plan, index) => (
+            <div className="plan" key={index}>
+
+                {plan.icon}
+                <span>{plan.name}</span>
+                <span>$ {plan.price}</span>
+        
+
+            <div className="features">
+                {plan.features.map((feature , index) => (
+                    <div className="feature">
+                        <img src={whitetick} alt="" />
+                        <span key={index}>{feature}</span>
+                    </div>
+                ))}
+            </div>
+            
+            <div>
+                <span>See more benefits </span>
+            </div>
+            <button className='btn'>Join now</button>
+            </div>
+         ))}
+        </div>
+
+    </div>
+  )
+}
+
+export default Plans
